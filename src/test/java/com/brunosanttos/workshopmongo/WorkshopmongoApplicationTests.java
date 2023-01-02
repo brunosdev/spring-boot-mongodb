1b81839d5d56551cfac0a5fb900f768a50d02962
Bruno Santos <brunosanttos@hotmail.com.br>
2023-01-02 15:07:01 -0300

    First commit

---
+++ src/test/java/com/brunosanttos/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunosanttos.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
