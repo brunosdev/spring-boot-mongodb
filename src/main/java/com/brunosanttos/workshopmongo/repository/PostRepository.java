f6900c06114cb5a01c70fd3f9f83afe4d61001e4
Bruno Santos <brunosanttos@hotmail.com.br>
2023-01-03 11:21:44 -0300

    Implementing query methods

--- src/main/java/com/brunosanttos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/brunosanttos/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,15 @@
 package com.brunosanttos.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.brunosanttos.workshopmongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
-	
+public interface PostRepository extends MongoRepository<Post, String> {
+
+	List<Post> findByTitleContainingIgnoreCase(String text);
+
 }
